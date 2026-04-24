c9c3ae9e68f54bb102ee7d935c748503d07ed19a
Semyon Aladin <sam625@mail.ru>
2026-04-24 14:57:25 +0300

    fix: alt of images now on interface language

--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -4,8 +4,8 @@
       <h2 class="title result-screen__title">{{ isError ? 'Что-то пошло не так' : 'Спасибо, ваш ответ отправлен' }}</h2>
       <p class="description result-screen__description">{{ isError ? 'Попробуйте заполнить форму позже' : 'Свяжемся с вами в ближайшее время' }}</p>
     </div>
-    <img v-if="isError" src="../assets/images/Error.png" alt="Error">
-    <img v-else src="../assets/images/Success.png" alt="Success">
+    <img v-if="isError" src="@/assets/images/Error.png" alt="Ошибка">
+    <img v-else src="@/assets/images/Success.png" alt="Успешно">
     <div class="result-screen__actions">
       <button v-if="isError" class="button button--secondary" @click="$emit('back')">
         Закрыть
